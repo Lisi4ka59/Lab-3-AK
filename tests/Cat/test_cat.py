67d4ec6aff1f9dbf1790f36926dc60c20b02db3b
Lisi4ka <122356446+Lisi4ka59@users.noreply.github.com>
2024-06-16 17:12:03 +0300

    Update test_cat.py

--- tests/Cat/test_cat.py
+++ tests/Cat/test_cat.py
@@ -1,1 +1,8 @@
-
+def test_answer():
+    with open("tests/Cat/Cat_answer", "r") as answer:
+        with open("output.txt", "r") as output:
+            assert answer.read() == output.read()
+    with open("tests/Cat/Cat_log", "r") as answer:
+        with open("log.txt", "r") as output:
+            assert answer.read() == output.read()
+          
