c4da9463047e232a83c83ff18c6a2e411e1b82ce
Lisi4ka <122356446+Lisi4ka59@users.noreply.github.com>
2024-06-20 18:53:17 +0300

    Update test_fibonnachi.py

--- tests/Fibonnachi/test_fibonnachi.py
+++ tests/Fibonnachi/test_fibonnachi.py
@@ -2,6 +2,3 @@ def test_answer():
     with open("tests/Fibonnachi/Fibonnachi_answer", "r") as answer:
         with open("output.txt", "r") as output:
             assert answer.read() == output.read()
-    with open("tests/Fibonnachi/Fibonnachi_log", "r") as answer:
-        with open("log.txt", "r") as output:
-            assert answer.read() == output.read()
