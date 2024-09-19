22725fffbdabb444cad706023e995823ca7ae711
Lisi4ka <122356446+Lisi4ka59@users.noreply.github.com>
2024-09-19 10:09:03 +0300

    Update test_hello_user_name.py

--- tests/Hello_user_name/test_hello_user_name.py
+++ tests/Hello_user_name/test_hello_user_name.py
@@ -2,3 +2,6 @@ def test_answer():
     with open("tests/Hello_user_name/Hello_user_name_answer", "r") as answer:
         with open("output.txt", "r") as output:
             assert answer.read() == output.read()
+    with open("tests/Hello_user_name/Hello_user_name_log", "r") as answer:
+        with open("log.txt", "r") as output:
+            assert answer.read() == output.read()
